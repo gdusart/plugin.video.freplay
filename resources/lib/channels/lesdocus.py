3a35676742df4bc1cb2ac41fac14736321d21f6a
greg <gdusart@gmail.com>
2017-08-30 22:43:10 +0200

    Code cleaning + unit tests

--- resources/lib/channels/lesdocus.py
+++ resources/lib/channels/lesdocus.py
@@ -1,22 +1,25 @@
-#-*- coding: utf-8 -*-
+# -*- coding: utf-8 -*-
 from bs4 import BeautifulSoup as bs
-import re
 import YDStreamExtractor
+
 
 title = ['Les docus']
 img = ['lesdocus']
 readyForUse = True
 bypass_cache = False
-debug = False
-
+test_mode = False
+max_pages_per_category = 50
 root_url = 'http://www.les-docus.com'
-forced_headers = {'user-agent':'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/60.0.3112.101 Safari/537.36'}
+
 
 def load_soup(url, cache_key):
-    if debug:
-        from urllib.request import Request, urlopen
-        q = Request(url, headers=forced_headers)
-        html = urlopen(q).read()
+    if test_mode:
+        import urllib2
+        force_headers = {
+            'user-agent':
+            'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/60.0.3112.101 Safari/537.36'}
+        req = urllib2.Request(url, headers=force_headers)
+        html = urllib2.urlopen(req).read()
     else:
         from resources.lib import utils
         file_path = utils.download_catalog(url, cache_key, bypass_cache, 'get', {}, True, {})
@@ -25,19 +28,22 @@ def load_soup(url, cache_key):
     return bs(html, 'html.parser')
 
 
-#**list_shows(channel,folder)**: Utilise pour recuperer la liste des menus.
-# Channel est toujours renseigne.Folder est un parametre remseigbne a none la 1ere fois et contient votre parametre les fois suivantes.
-# Retourne un tableau **Ligne=[ channel, parameter pour prochain passage dans fonction, Titre pour menu, image pour menu, action effectuee quand clic sur menu]**
-#_Dernier parametre : 'folder' : retour dans meme fonctions, 'shows' prochain passage est dans "list_videos"
+# **list_shows(channel,folder)**: Utilise pour recuperer la liste des menus.
+# Channel est toujours renseigne.Folder est un parametre remseigbne a none la 1ere fois et contient votre parametre
+# les fois suivantes.
+# Retourne un tableau **Ligne=[ channel, parameter pour prochain passage dans fonction,
+# Titre pour menu, image pour menu, action effectuee quand clic sur menu]**
+# _Dernier parametre : 'folder' : retour dans meme fonctions, 'shows' prochain passage est dans "list_videos"
+# noinspection PyPep8Naming
 def list_shows(channel, folder):
     categories = []
 
     print('listing categories for folder ' + folder)
     if folder != 'none':
-        #level 2
-        selector = '#%s li'%(folder)
+        # level 2
+        selector = '#%s li' % folder
     else:
-        #level 1
+        # level 1
         selector = 'ul.blog-menu > li'
 
     print('selector ' + selector)
@@ -45,40 +51,38 @@ def list_shows(channel, folder):
     soup = load_soup(root_url, root_url)
     menu_items = soup.select(selector)
 
-    #print('soup :%s'%(soup.prettify(encoding='utf-8')))
+    # print('soup :%s'%(soup.prettify(encoding='utf-8')))
 
     for item in menu_items:
-        #has_sub_menus = len(item.select('li')) != 0
+        # has_sub_menus = len(item.select('li')) != 0
         has_sub_menus = item.select_one('li') is not None
 
         if has_sub_menus:
             categories.append([channel, item.attrs['id'], item.select_one('a').getText().encode('utf-8'), '', 'folder'])
         else:
-            categories.append([channel, item.select_one('a').attrs['href'], item.select_one('a').getText().encode('utf-8'), '', 'shows'])
+            categories.append(
+                [channel, item.select_one('a').attrs['href'], item.select_one('a').getText().encode('utf-8'), '',
+                 'shows'])
 
-    print('Categories: %s'%(categories))
+    print('Categories: %s' % categories)
 
     return categories
 
 
-#**list_videos(channel,show_URL)**: Fonctionne de maniere similaire.
-# Retourne un tableau **Ligne=[ channel, param pour passage dans fonction getvideoURL, Titre pour menu, image pour menu, infoLabels, 'play']**
+# **list_videos(channel,show_URL)**: Fonctionne de maniere similaire.
+# Retourne un tableau **Ligne=[ channel, param pour passage dans fonction
+# getvideoURL, Titre pour menu, image pour menu, infoLabels, 'play']**
+# noinspection PyPep8Naming
 def list_videos(channel, show_name):
     videos = []
-    max_loops = 10
     loop_idx = 0
 
     next_page_link = show_name
-    while (next_page_link is not None and loop_idx < 10):
-        print('loading page %d: %s'%(loop_idx+1, next_page_link))
+    while next_page_link is not None and loop_idx < max_pages_per_category:
         next_page_link = get_posts(channel, next_page_link, videos)
-        loop_idx+=1
+        loop_idx += 1
 
-    print('Videos : %s'%(videos))
-    print('Next page link : %s'%(next_page_link))
-
-    #add link to next page >> does not work
-    #videos.append([channel, next_page_link, 'Next >>', '', 'shows'])
+    print('Videos : %s' % videos)
 
     return videos
 
@@ -90,7 +94,7 @@ def get_posts(channel, list_page, videos):
     posts = soup.select('.post')
     print('Found %d posts ' % (len(posts)))
 
-    #print(posts[0].prettify(encoding='utf-8'))
+    # print(posts[0].prettify(encoding='utf-8'))
     for post in posts:
         post_title = post.select_one('h2 a').getText().encode('utf-8')
         link = post.select_one('h2 a').attrs['href'].encode('utf-8')
@@ -100,43 +104,27 @@ def get_posts(channel, list_page, videos):
         info_labels = {
             "Title": post_title,
             "Plot": desc.getText().encode('utf-8') if desc is not None else '',
-            #"Aired": date,
-            #"Duration": duration,
-            #"Year": date[:4]
+            # "Aired": date,
+            # "Duration": duration,
+            # "Year": date[:4]
         }
         videos.append([channel, link, post_title, img_url, info_labels, 'play'])
 
     next_page_item = soup.select_one('.post-nav a.next')
-    next_page_link =  next_page_item.attrs['href'] if next_page_item is not None else None
+    next_page_link = next_page_item.attrs['href'] if next_page_item is not None else None
 
     return next_page_link
 
-#getVideoURL(channel,video_URL)**: Retourne l'URL qui devra etre lu par KODI
+
+# getVideoURL(channel,video_URL)**: Retourne l'URL qui devra etre lu par KODI
+# noinspection PyPep8Naming,PyUnusedLocal
 def getVideoURL(channel, video_url):
-    print('fetching URL for %s'%(video_url))
+    print('fetching URL for %s' % video_url)
 
     info = YDStreamExtractor.getVideoInfo(video_url, quality=3)
 
     if info is None:
-        print('YDStream did no return anything for URL %s' % (video_url))
+        print('YDStream did no return anything for URL %s' % video_url)
         return None
     else:
         return info.streamURL()
-
-
-def main():
-    # root_menu = (list_shows('lesdocus', 'none'))
-    # print('Root menu %s'%(root_menu))
-    #
-    # first_sub_menu = list_shows('lesdocus', root_menu[0][1])
-    # print('Sub menu %s'%(first_sub_menu))
-    #
-    # videos = list_videos('lesdocus', first_sub_menu[0][1])
-    # print('Videos: %s'%(videos))
-    print(getVideoURL('lesdocus', 'http://www.les-docus.com/new-york-face-aux-ouragans/'))
-    print(getVideoURL('lesdocus', 'http://www.les-docus.com/archi-du-sud/'))
-    print(getVideoURL('lesdocus', 'http://www.les-docus.com/cest-pas-sorcier-faire-un-disque-ca-vous-chante/'))
-
-
-if __name__ == "__main__":
-    main()
---
+++ resources/tests/test_lesdocus.py
@@ -0,0 +1,29 @@
+import unittest
+import resources.lib.channels.lesdocus as plugin
+import time
+
+
+class SimpleTest(unittest.TestCase):
+    def setUp(self):
+        plugin.test_mode = True
+
+    def test_1(self):
+        root_menu = (plugin.list_shows('lesdocus', 'none'))
+        print('Root menu %s' % root_menu)
+        self.assertNotEquals(0, len(root_menu))
+
+        first_sub_menu = plugin.list_shows('lesdocus', root_menu[0][1])
+        self.assertNotEquals(0, len(first_sub_menu))
+        print('Sub menu %s' % first_sub_menu)
+
+        start_time = time.time()
+        videos = plugin.list_videos('lesdocus', first_sub_menu[0][1])
+        elapsed_time = time.time() - start_time
+        self.assertNotEquals(0, len(videos))
+        print('Loaded %d Videos in %d seconds' % (len(videos), elapsed_time))
+
+    def test_url_resolution(self):
+        self.assertIsNotNone(plugin.getVideoURL('lesdocus', 'http://www.les-docus.com/new-york-face-aux-ouragans/'))
+        self.assertIsNotNone(plugin.getVideoURL('lesdocus', 'http://www.les-docus.com/archi-du-sud/'))
+        self.assertIsNotNone(
+            plugin.getVideoURL('lesdocus', 'http://www.les-docus.com/cest-pas-sorcier-faire-un-disque-ca-vous-chante/'))
